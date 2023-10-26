2f3287123914e255a71edc9edd4fd4498233bac8
girishdjagade <148955200+girishdjagade@users.noreply.github.com>
2023-10-26 07:30:51 +0000

    added datatype and  comparision taask

---
+++ basic_01/04_comparision.js
@@ -0,0 +1,12 @@
+// console.log("2" + 1);
+// console.log("2" > 1); 
+/*when i used + operator then 2 consider string but when i used > comparision then 2 consider number*/
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null);
+
+console.log("2" == 2);
+console.log("2" === 2);
---
+++ basic_01/datatype_summery.js
@@ -0,0 +1,5 @@
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id);
+console.log(id === anotherId);
