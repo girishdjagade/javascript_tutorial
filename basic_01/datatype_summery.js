4986e938bee66ad1c6466cb83cebc20fa0649e42
girishdjagade <148955200+girishdjagade@users.noreply.github.com>
2023-10-26 08:17:47 +0000

    added datatype summery task

--- basic_01/datatype_summery.js
+++ basic_01/datatype_summery.js
@@ -3,3 +3,17 @@ const anotherId = Symbol("123")
 
 console.log(id);
 console.log(id === anotherId);
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) Heap (Non Primitive)
+
+let userOne = {
+    email: "userone@gmail.com",
+    upi: "userOne@rbl"
+}
+let userTwo = userOne
+userTwo.email = "userTwo@gmail.com"
+
+console.log(userOne);
+console.log(userTwo);
