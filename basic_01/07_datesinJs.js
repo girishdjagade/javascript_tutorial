5ad4e1647c329ed8a005c3e57020f906e00fe046
girishdjagade <148955200+girishdjagade@users.noreply.github.com>
2023-10-26 13:29:22 +0000

    Added Date and Time task

---
+++ basic_01/07_datesinJs.js
@@ -0,0 +1,12 @@
+// Dates
+
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let createdDate = new Date(2023, 0, 23)
+let createdDate = new Date(2023, 0, 23, 5, 3)
+console.log(createdDate.toLocaleString());
